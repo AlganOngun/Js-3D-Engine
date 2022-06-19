9a7f2bb05304966a55f2198fcb2c57d381053893
Blank <68225247+AlganOngun@users.noreply.github.com>
2022-06-19 13:44:05 +0300

    Disabled devtools running by default

--- main.js
+++ main.js
@@ -20,8 +20,6 @@ function createWindow() {
     mainWindow.on('closed', function() {
         mainWindow = null
     })
-    
-    mainWindow.webContents.openDevTools();
 }
 
 app.on('ready', createWindow)
